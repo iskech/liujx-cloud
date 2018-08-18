500bd0dbc495e9ac0477527b4208bdfccbc79e5e
ljscraft <liujiaxing37@outlook.com>
2018-08-18 20:10:32 +0800

    调整修改eureka模块名后的bug

---
+++ cloud-eureka/src/main/java/com/scscloud/cloudeureka/CloudEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.scscloud.cloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//声明该应用为Eureka 注册中心
+@EnableEurekaServer
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+}
