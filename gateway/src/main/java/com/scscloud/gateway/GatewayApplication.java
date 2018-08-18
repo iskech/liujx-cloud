27f95adedeab2b521c0b73d9ab4e5e2b411813d7
ljscraft <liujiaxing37@outlook.com>
2018-08-18 20:05:08 +0800

    gateway 主配置类加入feign,eurekaclient组件 注解

--- gateway/src/main/java/com/scscloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/scscloud/gateway/GatewayApplication.java
@@ -2,8 +2,14 @@ package com.scscloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+//声明该应用为eureka client将网关服务注册到eureka注册中心
+@EnableDiscoveryClient
+//允许该应用使用Feign组件
+@EnableFeignClients
 public class GatewayApplication {
 
     public static void main(String[] args) {
